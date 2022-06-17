382f41bedd487319e96c7946d044172854e412ca
L1MeN9Yu <baal998.mophisto@gmail.com>
2022-06-17 14:11:20 +0800

    [Wallhaven] : APIKeyCollectionViewLayout

--- Apps/Wallhaven/Modules/App/Modules/AppMe/Sources/APIKey/APIKeyCollectionViewLayout.swift
+++ Apps/Wallhaven/Modules/App/Modules/AppMe/Sources/APIKey/APIKeyCollectionViewLayout.swift
@@ -5,4 +5,30 @@
 import Foundation
 import UIKit
 
-class APIKeyCollectionViewLayout: UICollectionViewCompositionalLayout {}
+class APIKeyCollectionViewLayout: UICollectionViewCompositionalLayout {
+    convenience init() {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(60)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitem: item,
+            count: 1
+        )
+        let section = NSCollectionLayoutSection(group: group)
+
+        let footerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(1.0))
+        let footer = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: footerSize, elementKind: UICollectionView.elementKindSectionFooter, alignment: .bottom)
+
+        section.boundarySupplementaryItems = [
+            footer,
+        ]
+        self.init(section: section)
+    }
+}
