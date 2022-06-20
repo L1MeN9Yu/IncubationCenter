e855938c77ea1a9c7b70abc714648e28af03bd6d
L1MeN9Yu <baal998.mophisto@gmail.com>
2022-06-20 10:56:00 +0800

    [Package] : BaseUI CollectionView add `Tap to Dismiss Keyboard`

--- Packages/UICore/BaseUI/Sources/BaseUI/View/CollectionView.swift
+++ Packages/UICore/BaseUI/Sources/BaseUI/View/CollectionView.swift
@@ -4,7 +4,15 @@
 
 import UIKit
 
-open class CollectionView: UICollectionView {}
+open class CollectionView: UICollectionView {
+    override public init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        setupTapToDismissKeyboard()
+    }
+
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
 
 public extension CollectionView {
     func register<CellClass: CollectionViewCell>(_ cellClass: CellClass.Type) {
@@ -15,4 +23,29 @@ public extension CollectionView {
         guard let cell = dequeueReusableCell(withReuseIdentifier: CellClass.cellID, for: indexPath) as? CellClass else { fatalError("\(CellClass.cellID) dequeue for \(indexPath) error") }
         return cell
     }
+}
+
+private extension CollectionView {
+    func setupTapToDismissKeyboard() {
+        guard isEnableTapToDismissKeyboard else { return }
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapGestureAction(tapGestureRecognizer:)))
+        tapGestureRecognizer.delegate = self
+        tapGestureRecognizer.cancelsTouchesInView = false
+        addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    @objc
+    func tapGestureAction(tapGestureRecognizer: UITapGestureRecognizer) {
+        endEditing(true)
+    }
+}
+
+extension CollectionView: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        true
+    }
+}
+
+extension CollectionView {
+    open var isEnableTapToDismissKeyboard: Bool { true }
 }
