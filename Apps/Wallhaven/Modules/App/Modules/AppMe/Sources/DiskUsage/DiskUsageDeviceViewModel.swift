94e964e556908e37b075405c6e73d90fdbff4244
L1MeN9Yu <baal998.mophisto@gmail.com>
2022-07-04 13:43:34 +0800

    [Wallhaven] : DiskUsage => Device Cell UI

--- Apps/Wallhaven/Modules/App/Modules/AppMe/Sources/DiskUsage/DiskUsageDeviceViewModel.swift
+++ Apps/Wallhaven/Modules/App/Modules/AppMe/Sources/DiskUsage/DiskUsageDeviceViewModel.swift
@@ -8,14 +8,14 @@ import Foundation
 class DiskUsageDeviceViewModel: ViewModel {
     let section = DiskUsageSection.device
 
-    private(set) var availablePercent: Double = 0
+    private(set) var usedPercent: Double = 0
     private(set) var appPercent: Double = 0
 }
 
 extension DiskUsageDeviceViewModel {
     func update(diskUsageInfo: DiskUsageInfo) {
         if diskUsageInfo.deviceTotal > 0 {
-            availablePercent = Double(diskUsageInfo.deviceAvailable) / Double(diskUsageInfo.deviceTotal)
+            usedPercent = Double(diskUsageInfo.deviceTotal - diskUsageInfo.deviceAvailable) / Double(diskUsageInfo.deviceTotal)
             appPercent = Double(diskUsageInfo.bundle + diskUsageInfo.container) / Double(diskUsageInfo.deviceTotal)
         }
     }
