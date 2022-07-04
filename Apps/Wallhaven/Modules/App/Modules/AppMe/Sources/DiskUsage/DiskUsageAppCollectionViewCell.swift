622d77243c1fff7003cddcf11a92da282a241333
L1MeN9Yu <baal998.mophisto@gmail.com>
2022-07-04 14:43:39 +0800

    [Wallhaven] : DiskUsage => App Cell UI

--- Apps/Wallhaven/Modules/App/Modules/AppMe/Sources/DiskUsage/DiskUsageAppCollectionViewCell.swift
+++ Apps/Wallhaven/Modules/App/Modules/AppMe/Sources/DiskUsage/DiskUsageAppCollectionViewCell.swift
@@ -7,6 +7,23 @@ import Foundation
 import UIKit
 
 class DiskUsageAppCollectionViewCell: CollectionViewCell {
+    private lazy var titleLabel: UILabel = .init(frame: .zero)
+        .x
+        .tintColor(.systemGray2)
+        .font(.preferredFont(forTextStyle: .footnote))
+        .instance
+    private lazy var valueLabel: UILabel = .init(frame: .zero)
+        .x
+        .textColor(.systemBlack)
+        .font(.preferredFont(forTextStyle: .title3))
+        .instance
+    private lazy var descriptionLabel: UILabel = .init(frame: .zero)
+        .x
+        .textColor(.systemGray4)
+        .font(.preferredFont(forTextStyle: .footnote))
+        .numberOfLines(0)
+        .instance
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
@@ -24,11 +41,24 @@ extension DiskUsageAppCollectionViewCell {
 }
 
 private extension DiskUsageAppCollectionViewCell {
-    func setup() {}
+    func setup() {
+        titleLabel.x.add(to: contentView)
+        valueLabel.x.add(to: contentView)
+        descriptionLabel.x.add(to: contentView)
+    }
 
-    func layout() {}
+    func layout() {
+        titleLabel.pin.left(8).top().margin(8).sizeToFit(.content)
+        valueLabel.pin.below(of: titleLabel).left(8).margin(8).sizeToFit(.content)
+        descriptionLabel.pin.below(of: valueLabel).left(8).margin(8).sizeToFit(.content)
+    }
 }
 
 extension DiskUsageAppCollectionViewCell {
-    func config(viewModel: DiskUsageAppViewModel) {}
+    func config(viewModel: DiskUsageAppViewModel) {
+        titleLabel.x.text(viewModel.titleText).done
+        valueLabel.x.text(viewModel.valueText).done
+        descriptionLabel.x.text(viewModel.descriptionText).done
+        setNeedsLayout()
+    }
 }
